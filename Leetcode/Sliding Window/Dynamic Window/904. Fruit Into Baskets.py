b8e84878d0aff81a3e45bfa5109fd5ebb24f4af3
Deepak-vikin <deepak5519u@gmail.com>
2026-06-26 10:16:56 +0530

    Sliding Window Dynamic

---
+++ Leetcode/Sliding Window/Dynamic Window/904. Fruit Into Baskets.py
@@ -0,0 +1,41 @@
+"""
+You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+Given the integer array fruits, return the maximum number of fruits you can pick.
+
+
+
+Example 1:
+
+Input: fruits = [1,2,1]
+Output: 3
+Explanation: We can pick from all 3 trees.
+
+"""
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left=0
+        f={}
+        m=0
+        for r in range(len(fruits)):
+            if fruits[r] in f:
+                f[fruits[r]]+=1
+            else:
+                f[fruits[r]]=1
+            while len(f)>2:
+                if f[fruits[left]]>1:
+                    f[fruits[left]]-=1
+                else:
+                    del f[fruits[left]]
+                left+=1
+            m=max(m,r-left+1)
+        return m
+obj=Solution()
+res=obj.totalFruit([1,2,1])
+print(res)

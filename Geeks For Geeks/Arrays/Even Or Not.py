99b6b177309472bed1fde421041e7ca5cbb3cc58
Deepak-vikin <deepak5519u@gmail.com>
2026-06-30 15:04:48 +0530

    Problem Solving

--- Geeks For Geeks/Arrays/Even Or Not.py
+++ Geeks For Geeks/Arrays/Even Or Not.py
@@ -1,4 +1,5 @@
-"""Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+"""
+Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
 
 Examples:
 
@@ -7,7 +8,8 @@ Output: false
 Explanation: The number is not divisible by 2, Odd number.
 Input: n = 44
 Output: true
-Explanation: The number is divisible by 2, Even number."""
+Explanation: The number is divisible by 2, Even number.
+"""
 
 class Solution:
     def isEven (self, n):

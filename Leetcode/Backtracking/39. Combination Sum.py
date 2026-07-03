caef9b22772197617e100c9e600080e68a34ab36
Deepak-vikin <deepak5519u@gmail.com>
2026-07-03 15:32:00 +0530

    Backtracking

---
+++ Leetcode/Backtracking/39. Combination Sum.py
@@ -0,0 +1,39 @@
+"""
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+"""
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res=[]
+        def findsoln(i,sum,arr):
+            if i>=n:
+                return
+            if sum==target:
+                res.append(arr)
+                return
+            if sum>target:
+                return
+            findsoln(i,sum+candidates[i],arr+[candidates[i]])
+            findsoln(i+1,sum,arr)
+            return
+        n=len(candidates)
+        findsoln(0,0,[])
+        return res
+obj=Solution()
+res=obj.combinationSum([2,3,6,7],7)
+print(res)
---
+++ Leetcode/Math/1037. Valid Boomerang.py
@@ -0,0 +1,31 @@
+"""
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+"""
+
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        x1,y1=points[0]
+        x2,y2=points[1]
+        x3,y3=points[2]
+
+        a=x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)
+        if a!=0:
+            return True
+        else:
+            return False
+obj=Solution()
+res=obj.isBoomerang([[1,1],[2,2],[3,3]])
+print(res)

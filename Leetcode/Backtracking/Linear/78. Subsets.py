99cc5e18661576fb06c545b095ebfb867dad791e
Deepak-vikin <deepak5519u@gmail.com>
2026-07-06 14:51:29 +0530

    Backtracking

---
+++ Leetcode/Backtracking/Linear/78. Subsets.py
@@ -0,0 +1,30 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+"""
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        def solve(i,path):
+            if i==len(nums):
+                res.append(path)
+                return
+            if i>=len(nums):
+                return
+            solve(i+1,path+[nums[i]])
+            solve(i+1,path)
+        solve(0,[])
+        res.sort()
+        return res
+obj=Solution()
+res=obj.subsets([1,2,2])
+print(res)

5f4f8d80cb8d15461bb3dbbcc7957cb7ab0d492e
Deepak-vikin <deepak5519u@gmail.com>
2026-07-10 15:21:36 +0530

    Prefix Sum

---
+++ Leetcode/Prefix Sum/523. Continuous Subarray Sum.py
@@ -0,0 +1,37 @@
+"""
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+"""
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pref=[]
+        f={0:-1}
+        pref.append(nums[0])
+        for i in range(1,len(nums)):
+            pref.append(pref[i-1]+nums[i])
+        for i in range(len(pref)):
+            if pref[i]%k not in f:
+                f[pref[i]%k]=i
+        for i in range(len(pref)):
+            if pref[i]%k in f:
+                if i-f[pref[i]%k]>=2:
+                    return True
+        return False
+obj=Solution()
+res=obj.checkSubarraySum([23,2,4,6,7],6)
+print(res)
---
+++ Leetcode/Prefix Sum/974. Subarray Sums Divisible by K.py
@@ -0,0 +1,30 @@
+"""
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+"""
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        f={0:1}
+        pref=0
+        count=0
+        for i in range(len(nums)):
+            pref+=nums[i]
+            rem=pref%k
+            if rem not in f:
+                f[rem]=1
+            else:
+                count+=f[rem]
+                f[rem]+=1
+        return count
+obj=Solution()
+print(obj.subarraysDivByK([4,5,0,-2,-3,1]))

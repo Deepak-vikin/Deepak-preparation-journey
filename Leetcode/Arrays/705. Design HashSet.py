95b0fa090a321e850a5a64498f4edef37a26e5d0
Deepak-vikin <deepak5519u@gmail.com>
2026-07-29 12:24:52 +0530

    Daily Solving

---
+++ Leetcode/Arrays/705. Design HashSet.py
@@ -0,0 +1,61 @@
+"""
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+"""
+class MyHashSet:
+
+    def __init__(self):
+        self.arr=[-1]*((10**6)+1)
+    def add(self, key: int) -> None:
+        self.arr[key]=1
+    def remove(self, key: int) -> None:
+        self.arr[key]=-1
+    def contains(self, key: int) -> bool:
+        if self.arr[key]!=-1:
+            return True
+        return False
+obj=MyHashSet()
+obj.add(1)
+obj.add(2)
+obj.add(3)
+obj.add(4)
+obj.add(5)
+obj.remove(1)
+obj.remove(2)
+param_3=obj.contains(3)
+param_4=obj.contains(4)
+param_5=obj.contains(5)
+print(param_3)
+print(param_4)
+print(param_5)
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)

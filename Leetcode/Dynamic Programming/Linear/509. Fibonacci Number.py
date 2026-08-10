e1e6d8f779c1e4932c00c30366bb5f050aeea538
Deepak-vikin <deepak5519u@gmail.com>
2026-08-10 09:03:41 +0530

    Dp

---
+++ Leetcode/Dynamic Programming/Linear/509. Fibonacci Number.py
@@ -0,0 +1,28 @@
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+"""
+class Solution:
+    def fib(self, n: int) -> int:
+        dp=[0]*(n+1)
+        if n<=0:
+            return 0
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+obj = Solution()
+res = obj.fib(5)
+print(res)

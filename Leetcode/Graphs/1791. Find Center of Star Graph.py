5fa44e71d149050e89be00eacd0067f1b4da4487
Deepak-vikin <deepak5519u@gmail.com>
2026-08-31 09:57:58 +0530

    BFS

--- Leetcode/Graphs/1791. Find Center of Star Graph.py
+++ Leetcode/Graphs/1791. Find Center of Star Graph.py
@@ -2,11 +2,7 @@
 There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
 
 You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
-
-
-
 Example 1:
-
 
 Input: edges = [[1,2],[2,3],[4,2]]
 Output: 2

6d8d7e5eee038b4931fc982f247904d4587c3935
Deepak-vikin <deepak5519u@gmail.com>
2026-09-02 09:36:23 +0530

    DFS Tree

---
+++ Leetcode/Tree/Depth First Search/Traversals/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,31 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Explanation:
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return res
+obj=Solution()
+res=obj.inorderTraversal([1,2,3,4,5,null,8,null,null,6,7,9])
+print(res)
